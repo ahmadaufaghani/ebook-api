623dcc055b9e7a126d0395fc65c95e848db6c475
ahmadaufaghani <3103120010@student.smktelkom-pwt.sch.id>
2022-10-24 14:06:14 +0700

    second commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::get('halo', function() {
+//     return [
+//         'status'=>201,
+//         'result'=>[
+//             "nama" => "Ghani",
+//             "kelas"=> "XII",
+//             "jurusan"=> "RPL"
+//         ],
+//         'error'=>[]
+//     ];
+// });
+
+// Route::get('halocontroller', [HeloController::class, 'index']);
+// Route::resource('halocontroller', HeloController::class);
+// Route::resource('siswacontroller', SiswaController::class);
+
+Route::resource('authors', AuthorController::class);
