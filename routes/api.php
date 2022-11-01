75a9c77a5e2d0debbe70e6490a534322f57923a9
ahmadaufaghani <3103120010@student.smktelkom-pwt.sch.id>
2022-11-01 15:00:18 +0700

    auth

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,5 +37,18 @@ use App\Http\Controllers\BookController;
 // Route::resource('halocontroller', HeloController::class);
 // Route::resource('siswacontroller', SiswaController::class);
 
-Route::resource('authors', AuthorController::class);
-Route::resource('books', BookController::class);
+// Route::resource('books', BookController::class);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('books', [BookController::class, 'index']);
+Route::get('authors', [AuthorController::class, 'index']);
+Route::middleware('auth:sanctum')->group(function() {
+    Route::post('/books', [BookController::class, 'store']);
+    Route::put('/books/{id}', [BookController::class, 'update']);
+    Route::delete('/books/{id}', [BookController::class, 'destroy']);
+    Route::post('/authors',[AuthorController::class, 'store']);
+    Route::put('/authors/{id}',[AuthorController::class, 'update']);
+    Route::delete('/authors/{id}',[AuthorController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
